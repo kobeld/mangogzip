fb159a9c73821be395d07eb189b333cbf696b66b
kobeld <kobeld@163.com>
2012-06-27 17:41:13 +0800

    Initialize

---
+++ zipper.go
@@ -0,0 +1,29 @@
+package mangogzip
+
+import (
+        "bytes"
+        "compress/gzip"
+        . "github.com/paulbellamy/mango"
+        "strings"
+)
+
+func Zipper(env Env, app App) (status Status, headers Headers, body Body) {
+        status, headers, body = app(env)
+
+        ae := env.Request().Header.Get("Accept-Encoding")
+        if strings.Contains(ae, "gzip") {
+
+                if headers == nil {
+                        headers = Headers{}
+                }
+                headers.Set("Content-Encoding", "gzip")
+
+                buff := bytes.NewBuffer(nil)
+                gw := gzip.NewWriter(buff)
+                gw.Write([]byte(body))
+                gw.Close()
+                body = Body(buff.String())
+        }
+
+        return
+}
