1f9944948e19104ba4215a03a43a219211e6a4f3
kobeld <kobeld@163.com>
2013-03-28 23:51:14 +0800

    Return header content type

--- zipper.go
+++ zipper.go
@@ -1,29 +1,36 @@
 package mangogzip
 
 import (
-        "bytes"
-        "compress/gzip"
-        . "github.com/paulbellamy/mango"
-        "strings"
+	"bytes"
+	"compress/gzip"
+	. "github.com/paulbellamy/mango"
+	"strings"
 )
 
 func Zipper(env Env, app App) (status Status, headers Headers, body Body) {
-        status, headers, body = app(env)
+	status, headers, body = app(env)
 
-        ae := env.Request().Header.Get("Accept-Encoding")
-        if strings.Contains(ae, "gzip") {
+	ae := env.Request().Header.Get("Accept-Encoding")
 
-                if headers == nil {
-                        headers = Headers{}
-                }
-                headers.Set("Content-Encoding", "gzip")
+	if headers == nil {
+		headers = Headers{}
+		headers.Set("Content-Type", "text/html")
+	}
 
-                buff := bytes.NewBuffer(nil)
-                gw := gzip.NewWriter(buff)
-                gw.Write([]byte(body))
-                gw.Close()
-                body = Body(buff.String())
-        }
+	contentType := headers.Get("Content-Type")
+	if contentType == "" {
+		headers.Set("Content-Type", "text/html; charset=utf8")
+	}
 
-        return
+	if strings.Contains(ae, "gzip") {
+		headers.Set("Content-Encoding", "gzip")
+
+		buff := bytes.NewBuffer(nil)
+		gw := gzip.NewWriter(buff)
+		gw.Write([]byte(body))
+		gw.Close()
+		body = Body(buff.String())
+	}
+
+	return
 }
